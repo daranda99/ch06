aefc21e0ef79d2e0fd4771c735f13b53dfccfd6b
daranda99 <34823449+daranda99@users.noreply.github.com>
2017-12-30 01:07:08 -0800

    Update and rename LICENSE.md to angryBoss.rb

---
+++ LICENSE.md/angryBoss.rb
@@ -0,0 +1,9 @@
+irb(main):001:0> puts 'What do you want!?'
+What do you want!?
+=> nil
+irb(main):002:0> want = gets.chomp
+a raise
+=> "a raise"
+irb(main):004:0> puts 'WHAT DO YOU MEAN "I WANT ' + want.upcase + ' " ?!? YOU\'RE FIRED!!'
+WHAT DO YOU MEAN "I WANT A RAISE " ?!? YOU'RE FIRED!!
+=> nil
